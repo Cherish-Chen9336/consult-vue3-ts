96c9fd210e140e1a97e9575bca4d6f892069a70c
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-04-06 21:19:51 +0800

    feat: cp-nav-bar 回退功能

--- src/components/CpNavBar.vue
+++ src/components/CpNavBar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
 // 1. 通过 props 函数 动态设置标题和 右侧文字
 defineProps<{
   title?: string
@@ -14,12 +16,24 @@ const emit = defineEmits<{
 const onClickRight = () => {
   emit('click-right')
 }
+
+const router = useRouter()
+
+// 3. 回退， 了解 history.state 信息 监听箭头的点击事件
+const onClickLeft = () => {
+  if (history.state?.back) {
+    router.back()
+  } else {
+    router.push('/')
+  }
+}
 </script>
 
 <template>
   <!-- 需要 固定定位， 左侧箭头，中间标题， 右侧文字 -->
   <van-nav-bar
     :title="title"
+    @click-left="onClickLeft"
     left-arrow
     :right-text="rightText"
     @click-right="onClickRight"
