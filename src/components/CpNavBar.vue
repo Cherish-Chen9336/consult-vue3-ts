d689882b247513803589842e2a168aa740008e16
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-04-06 19:02:06 +0800

    feat: cp-nav-bar 功能标题和右侧文字点击和自定事件

--- src/components/CpNavBar.vue
+++ src/components/CpNavBar.vue
@@ -1,8 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 1. 通过 props 函数 动态设置标题和 右侧文字
+defineProps<{
+  title?: string
+  rightText?: string
+}>()
+
+// 2. 通过 emit 函数触发自定义事件 (点击右侧按钮)
+
+const emit = defineEmits<{
+  (e: 'click-right'): void
+}>()
+
+const onClickRight = () => {
+  emit('click-right')
+}
+</script>
 
 <template>
   <!-- 需要 固定定位， 左侧箭头，中间标题， 右侧文字 -->
-  <van-nav-bar fixed title="标题" right-text="注册" left-arrow></van-nav-bar>
+  <van-nav-bar
+    :title="title"
+    left-arrow
+    :right-text="rightText"
+    @click-right="onClickRight"
+  ></van-nav-bar>
 </template>
 
 <style lang="scss" scoped>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const fn = () => console.log('fn')
+</script>
 
 <template>
   <div class="login-page">
-    <cp-nav-bar></cp-nav-bar>
+    <cp-nav-bar title="登录" right-text="注册" @click-right="fn"></cp-nav-bar>
     <van-checkbox>复选框</van-checkbox>
   </div>
 </template>
