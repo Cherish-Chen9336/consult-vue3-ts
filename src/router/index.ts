a0676429ac87aa088d77885656a606307486f48e
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-04-08 17:06:44 +0800

    feat: 布局容器-标题设置

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,8 @@ const router = createRouter({
   routes: [
     {
       path: '/login',
-      component: () => import('@/views/Login/index.vue')
+      component: () => import('@/views/Login/index.vue'),
+      meta: { title: '登录' }
     },
     {
       path: '/',
@@ -16,19 +17,23 @@ const router = createRouter({
       children: [
         {
           path: '/home',
-          component: () => import('@/views/Home/index.vue')
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: '首页' }
         },
         {
           path: '/article',
-          component: () => import('@/views/Article/index.vue')
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: '健康百科' }
         },
         {
           path: '/notify',
-          component: () => import('@/views/Notify/index.vue')
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: '消息通知' }
         },
         {
           path: '/user',
-          component: () => import('@/views/User/index.vue')
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: '个人中心' }
         }
       ]
     }
@@ -45,6 +50,11 @@ router.beforeEach((to) => {
   const whiteList = ['/login']
   // 判断如果不存在token， 或不在白名单，则需要拦截
   if (!store.user?.token && !whiteList.includes(to.path)) return '/login'
+})
+
+// 后置导航守卫
+router.afterEach((to) => {
+  document.title = `${to.meta.title}-优医问诊`
 })
 
 export default router
