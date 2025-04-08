5aa675d374b7695a38b49805021fd787c0802f1f
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-04-08 16:39:55 +0800

    feat：访问权限控制

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -35,5 +36,15 @@ const router = createRouter({
 })
 
 // console.log(import.meta)
+
+// 全局前置导航
+router.beforeEach((to) => {
+  // 获取token
+  const store = useUserStore()
+  // 声明白名单
+  const whiteList = ['/login']
+  // 判断如果不存在token， 或不在白名单，则需要拦截
+  if (!store.user?.token && !whiteList.includes(to.path)) return '/login'
+})
 
 export default router
