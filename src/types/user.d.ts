6bb96a9982ce0eb2fda40fbdc8634c7322cf1052
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-04-08 18:31:35 +0800

    feat:个人中心-用户信息类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -18,3 +18,28 @@ export type CodeType =
   | 'changeMobile'
   | 'forgetPassword'
   | 'bindMobile'
+
+// 个人信息
+type OmitUser = Omit<User, 'token'>
+
+export type UserInfo = OmitUser & {
+  /**关注数量 */
+  likeNumber: number
+  /**收藏数量 */
+  collectionNumber: number
+  /**我的总积分 */
+  score: number
+  /**优惠券数量 */
+  couponNumber: number
+  /**订单信息 */
+  orderInfo: {
+    /**待付款 */
+    paidNumber: number
+    /**待发货 */
+    receivedNumber: number
+    /**待收货 */
+    shippedNumber: number
+    /**已完成 */
+    finishedNumber: number
+  }
+}
